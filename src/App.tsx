80b2b340e7b2943f90d77b361c84c926d8859a91
Juan Dominguez <juanignaciodom3@gmail.com>
2023-09-21 22:40:12 -0300

    test env netlify

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React | {import.meta.env.VITE_API_URL}</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
