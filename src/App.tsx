9b34d3acf638ad9da45c53c17e813c56028b4e3a
Juan Dominguez <juanignaciodom3@gmail.com>
2023-09-23 14:37:45 -0300

    ignore .env file

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-import { Routes, Route, Outlet, Link } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./components/Home";
+import Layout from "./components/Layout";
 
 export default function App() {
   return (
@@ -14,46 +16,6 @@ export default function App() {
         <Route path="*" element={<NoMatch />} />
       </Route>
     </Routes>
-  );
-}
-
-function Layout() {
-  return (
-    <div>
-      {/* A "layout route" is a good place to put markup you want to
-          share across all the pages on your site, like navigation. */}
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/dashboard">Dashboard</Link>
-          </li>
-          <li>
-            <Link to="/nothing-here">Nothing Here</Link>
-          </li>
-        </ul>
-      </nav>
-
-      <hr />
-
-      {/* An <Outlet> renders whatever child route is currently active,
-          so you can think about this <Outlet> as a placeholder for
-          the child routes we defined above. */}
-      <Outlet />
-    </div>
-  );
-}
-
-function Home() {
-  return (
-    <div>
-      <h2>Home</h2>
-    </div>
   );
 }
 
---
+++ src/components/Layout.tsx
@@ -0,0 +1,30 @@
+import { Link, Outlet } from "react-router-dom";
+
+function Layout() {
+    return (
+        <main>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                    <li>
+                        <Link to="/dashboard">Dashboard</Link>
+                    </li>
+                    <li>
+                        <Link to="/nothing-here">Nothing Here</Link>
+                    </li>
+                </ul>
+            </nav>
+
+            <hr />
+
+            <Outlet />
+        </main>
+    );
+}
+
+export default Layout
