a5964cd6806192f6b588801cf324329df75e1e24
Juan Dominguez <juanignaciodom3@gmail.com>
2023-10-09 15:32:13 -0300

    add await in apiLoader

--- src/loaders/apiLoader.ts
+++ src/loaders/apiLoader.ts
@@ -3,7 +3,7 @@ import { defer } from "react-router-dom";
 import createApiService from "../api/api.service";
 
 async function apiLoader() {
-    const apiStatusPromise = createApiService().checkApiStatus()
+    const apiStatusPromise = await createApiService().checkApiStatus()
 
     return defer({
         apiStatus: apiStatusPromise,
